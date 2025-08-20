27fefc846c0f0cc596a4c2b4f8f2c96bda8a8da2
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-08-20 15:58:25 -0400

    Initial Commit

---
+++ Runner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Runner
+//
+//  Created by Edward Ogbalor on 2025-08-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Runner/RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerApp.swift
+//  Runner
+//
+//  Created by Edward Ogbalor on 2025-08-20.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
