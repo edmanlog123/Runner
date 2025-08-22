c9604520ed2c026f14226b8b31dcdab2f27af2d2
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-08-22 16:47:27 -0400

    Fixed key again

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -4,7 +4,9 @@ import GoogleMaps
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyBiC38eJ70n-yFI3yv5tojK2VGb1C5cg38")
+        if let key = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String {
+            GMSServices.provideAPIKey(key)
+        }
         return true
     }
 }
